acd10d77b6726293c9f3a4701fc88a03a8c5b73d
Masamoto Miyata <miyata@sincere-co.com>
2013-06-11 12:54:01 +0900

    Implemetend some utility methods.

--- jquery.rect.js
+++ jquery.rect.js
@@ -25,16 +25,49 @@ Rect.prototype.center = function(){
     return {top: this.offset.top + this.size.height / 2, left: this.offset.left + this.size.width / 2};
 };
 
+Rect.prototype.contains = function(rect){
+    return (
+        rect.offset.top >= this.offset.top &&
+        rect.offset.left >= this.offset.left &&
+        (rect.offset.top + rect.size.height) <= (this.offset.top + this.size.height) &&
+        (rect.offset.left + rect.size.width) <= (this.offset.left + this.size.width)
+    );
+};
+
+Rect.prototype.intersects = function(rect){
+    var startLeft = Math.max(this.offset.left, rect.offset.left);
+    var startTop = Math.max(this.offset.top, rect.offset.top);
+    var endLeft = Math.min(this.offset.left + this.size.width, rect.offset.left + rect.size.width);
+    var endTop = Math.min(this.offset.top + this.size.height, rect.offset.top + rect.size.height);
+
+    var width = endLeft - startLeft;
+    var height = endTop - startTop;
+    if (width > 0 && height > 0) {
+        return new Rect(width, height, startTop, startLeft);
+    } else {
+        return false;
+    }
+};
+
+Rect.prototype.equals = function(rect){
+    return (
+        rect.offset.top === this.offset.top &&
+        rect.offset.left === this.offset.left &&
+        rect.size.height === this.size.height &&
+        rect.size.width === this.size.width
+    );
+};
 
 $.fn.extend({
     rect: function(rect){
         if(rect !== undefined) //setter
         {
-            this.offset(rect.offset);
-            this.outerWidth(rect.size.width);
-            this.outerHeight(rect.size.height);
-
-            return this;
+            return this.each(function(){
+                var elem = $(this);
+                elem.offset(rect.offset);
+                elem.outerWidth(rect.size.width);
+                elem.outerHeight(rect.size.height);
+            });
         }
         else
         {
@@ -71,13 +104,11 @@ $.fn.extend({
     center: function(offset){
         if(offset !== undefined) //setter
         {
-            this.each(function(){
+            return this.each(function(){
                 var elem = $(this);
                 var newOffset = {top:offset.top - elem.outerHeight() / 2, left:offset.left - elem.outerWidth() / 2};
                 elem.offset(newOffset);
             });
-
-            return this;
         }
         else
         {
--- test/qunit.js
+++ test/qunit.js
@@ -5,14 +5,14 @@ test( "Rect", function() {
     var rect;
 
     rect = div.rect();
-    equal(rect.offset.top, 10);
-    equal(rect.offset.left, 30);
-    equal(rect.size.width, 100);
-    equal(rect.size.height, 1000);
+    strictEqual(rect.offset.top, 10);
+    strictEqual(rect.offset.left, 30);
+    strictEqual(rect.size.width, 100);
+    strictEqual(rect.size.height, 1000);
 
     var center = rect.center();
-    equal(center.top, 10 + (1000/2));
-    equal(center.left, 30 + (100/2));
+    strictEqual(center.top, 10 + (1000/2));
+    strictEqual(center.left, 30 + (100/2));
 
     var win = $(window);
     rect = win.rect();
@@ -25,8 +25,61 @@ test( "Rect", function() {
     var divForSetter = $('<div />').appendTo('body');
     divForSetter.rect(div.rect());
 
-    equal(divForSetter.offset().top, 10);
-    equal(divForSetter.offset().left, 30);
-    equal(divForSetter.width(), 100);
-    equal(divForSetter.height(), 1000);
+    strictEqual(divForSetter.offset().top, 10);
+    strictEqual(divForSetter.offset().left, 30);
+    strictEqual(divForSetter.width(), 100);
+    strictEqual(divForSetter.height(), 1000);
+});
+
+test( "contains", function() {
+    var div1 = $('<div />').appendTo('body');
+    div1.width(100).height(100).offset({top:0, left:0});
+
+    var div2 = $('<div />').appendTo('body');
+
+    //completly contains
+    div2.width(90).height(90).offset({top:5, left:5});
+    strictEqual(div1.rect().contains(div2.rect()), true);
+
+    //out
+    div2.width(90).height(90).offset({top:11, left:11});
+    strictEqual(div1.rect().contains(div2.rect()), false);
+
+    //too big width
+    div2.width(110).height(100).offset({top:0, left:0});
+    strictEqual(div1.rect().contains(div2.rect()), false);
+
+    //too big height
+    div2.width(100).height(110).offset({top:0, left:0});
+    strictEqual(div1.rect().contains(div2.rect()), false);
+
+    //too big both
+    div2.width(110).height(110).offset({top:0, left:0});
+    strictEqual(div1.rect().contains(div2.rect()), false);
+
+    //width equal
+    div2.width(100).height(90).offset({top:0, left:0});
+    strictEqual(div1.rect().contains(div2.rect()), true);
+
+    //height equal
+    div2.width(90).height(100).offset({top:0, left:0});
+    strictEqual(div1.rect().contains(div2.rect()), true);
+
+    //both equal
+    div2.width(100).height(100).offset({top:0, left:0});
+    strictEqual(div1.rect().contains(div2.rect()), true);
+});
+
+
+test( "intersects", function() {
+    var div1 = $('<div />').appendTo('body');
+    div1.width(100).height(100).offset({top:0, left:0});
+
+    var div2 = $('<div />').appendTo('body');
+
+    div2.width(100).height(100).offset({top:10, left:10});
+    strictEqual(div1.rect().intersects(div2.rect()).equals(new Rect(90, 90, 10, 10)), true);
+
+    div2.width(100).height(100).offset({top:100, left:100});
+    strictEqual(div1.rect().intersects(div2.rect()), false);
 });
